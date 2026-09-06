6c4254c1bd432af0a573a0dc47dae609bb6e2205
Barın Güzeldemirci <baringuzeldemir@gmail.com>
2026-09-06 19:36:15 +0300

    add project url

--- gobrush/__init__.py
+++ gobrush/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import annotations
 
 __version__ = "0.1.0"
-__app_id__ = "io.github.gobrush.GoBrush"
+__app_id__ = "io.github.word_sys.GoBrush"
--- tests/test_scaffolding.py
+++ tests/test_scaffolding.py
@@ -8,7 +8,7 @@ class TestScaffolding(unittest.TestCase):
         self.assertEqual(gobrush.__version__, "0.1.0")
 
     def test_app_id(self) -> None:
-        self.assertEqual(gobrush.__app_id__, "io.github.gobrush.GoBrush")
+        self.assertEqual(gobrush.__app_id__, "io.github.word_sys.GoBrush")
 
 
 if __name__ == "__main__":
